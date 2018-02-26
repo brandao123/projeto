31c061ca1cea81836a10559f48919179ac86134d
gabriel <gabrielcandidobrandao@gmail.com>
2018-02-26 18:33:08 -0300

    adicionando

--- celular.php
+++ celular.php
@@ -20,27 +20,24 @@ include'head.php';
 <?php
 include "conexao.php";
 
-$sql = "SELECT * FROM produto";
+$sql = "SELECT * FROM celular";
 $result = $conn->query($sql);
 
 if($result->num_rows>0){
   while($row = $result->fetch_assoc()){
-    // echo $row['nome']." - R$ ".$row['preco']." - ". $row['abrangencia']."<br/>";
-   
+    // echo "<img class='card-img-top'src=".$row['foto']."alt='Card image cap'>";
+    
+    echo "<div class='col-sm'>";
+    echo "<div class='card' style='width: 18rem'>";
 
-
- 
-
-    echo ";<div class='col-sm'>";
-     echo "; <div class='card' style='width: 18rem;'>";
-  echo ";<img class='card-img-top' src='produtos/".$row['foto']."alt='Card image cap'>";
-  echo ";<div class='card-body'>";
-    echo ";<h5 class='card-title'>".$row['nome']."</h5>";
-    echo ";<p class='card-text'> ".$row['descricao']." </p>";
-    echo " ;<a href='#' class='btn btn-outline-dark'>Pesquisar</a>";
-  echo "</div>";
-echo"</div>";
-   echo"</div>";
+   echo "<img src='produtos/".$row['foto']."'>";
+    echo "<div class='card-body'>";
+    echo "<h5 class='card-title'>".$row['nome']."</h5>";
+    echo "<p class='card-text'> ".$row['descricao']." </p>";
+    echo " <a href='#' class='btn btn-outline-dark'>Comprar</a>";
+    echo "</div>";
+    echo"</div>";
+    echo"</div>";
 
 
   
--- site.sql
+++ site.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 29-Jan-2018 às 23:29
--- Versão do servidor: 10.1.29-MariaDB
--- PHP Version: 7.1.12
+-- Generation Time: 22-Fev-2018 às 23:53
+-- Versão do servidor: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,6 +19,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `site`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `celular`
+--
+
+CREATE TABLE `celular` (
+  `id` int(4) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_bin NOT NULL,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `categoria` varchar(255) COLLATE utf8_bin NOT NULL,
+  `foto` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `celular`
+--
+
+INSERT INTO `celular` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
+(1, 'Iphone 6', 'Rose', 'celular', 'cel1.jpg'),
+(2, 'Iphone 6 Plus', 'Cinza Espacial', 'celular', 'cel2.jpg'),
+(3, 'Iphone 8', 'Branco', 'celular', 'cel3.jpg');
 
 -- --------------------------------------------------------
 
@@ -39,6 +60,29 @@ CREATE TABLE `cliente` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `computador`
+--
+
+CREATE TABLE `computador` (
+  `id` int(4) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_bin NOT NULL,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `categoria` varchar(255) COLLATE utf8_bin NOT NULL,
+  `foto` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `computador`
+--
+
+INSERT INTO `computador` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
+(1, 'Notebook Asus', 'I5, 8gb de memoria 1TB', 'computador', 'pc1.jpg'),
+(2, 'Notebook Sansumg', 'I5, 8gb 1tb', 'computador', 'pc2.jpg'),
+(3, 'Notebook Dell', 'I3, 8gb 1tb', 'computador', 'pc3.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `fucionario`
 --
 
@@ -52,10 +96,10 @@ CREATE TABLE `fucionario` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produto`
+-- Estrutura da tabela `informatica`
 --
 
-CREATE TABLE `produto` (
+CREATE TABLE `informatica` (
   `id` int(4) NOT NULL,
   `nome` varchar(255) COLLATE utf8_bin NOT NULL,
   `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -64,37 +108,103 @@ CREATE TABLE `produto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Extraindo dados da tabela `produto`
+-- Extraindo dados da tabela `informatica`
 --
 
-INSERT INTO `produto` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
-(2, 'iphone6', 'Marca: Apple Sistema operacional: iOS Tamanho da tela: Tela de 4,7 polegadas ResoluÃ§Ã£o da cÃ¢mera traseira: 5 megapixels Recurso: 4G LTE', 'celular', 'cel1.jpg'),
-(3, 'iphone6plus', 'Marca: Apple Sistema operacional: iOS Autonomia da bateria: 24 horas de conversaÃ§Ã£o Peso: 172 gramas MemÃ³ria interna: 16 GB NÃºmero de chips: 1 chip', 'celular', 'cel2.jpg'),
-(5, 'pendrive', 'Pen Drives 8 GB SanDisk ', 'informatica', 'info1.jpg'),
-(6, 'cartucho', 'cartucho tinta preta', 'informatica', 'info2.jpg'),
-(7, 'pendrivee', '8gb', 'informatica', 'info3.jpg'),
-(8, 'illuminati', 'Wilson conseguiu inverter todas as minhas polaridades mentais, como se me tivessem arrastado atravÃ©s do infinito.â€ Philip K. Dick â€œNada Ã© verdade. Tudo Ã© permissÃ­vel.â€ Hassan I Sabbah, fundador dos Illuminati em 1090 SerÃ¡ que toda a histÃ³ria n', 'livro', 'livro1.jpg'),
-(9, 'imaginacao', 'Em A imaginaÃ§Ã£o, encontra-se um Sartre prÃ©-Existencialismo e prÃ©-Segunda Guerra Mundial, fazendo uso de seus estudos sobre a fenomenologia - mÃ©todo desenvolvido pelo filÃ³sofo alemÃ£o Edmund Husserl ', 'livro', 'livro2.jpg'),
-(10, 'virando', 'Tudo se inicia quando AtÃ­lio, um espÃ­rito maligno e poderoso, lÃ­der de uma quadrilha de criminosos no submundo astral, decide que Mizael, seu protegido, deve reencarnar para levar o mal Ã  Terra. Afinal, o crime era a Ãºnica coisa que ele sabia fazer.', 'livro', 'livro3.jpg'),
-(11, 'azus', 'Notebook Asus Vivobook X510UA-BR485T Intel Core i5 4GB 1TB Tela Nano Edge 15,6 Windows 10 - Vermelho', 'notebook', 'pc1.jpg'),
-(12, 'iphone8', 'iPhone 8 Apple 64GB 4G Tela 4,7â€ - Retina CÃ¢m 12MP + Selfie 7MP iOS 11 Proc. Chip A11', 'celular', 'cel3.jpg'),
-(13, 'camisaA', 'Camisa Preta, GG', 'camisa', 'camisa1.jpg'),
-(14, 'camisaB', 'Camisa Preta, GG', 'camisa', 'camisa2.jpg'),
-(15, 'camisaC', 'Camisa Preta, GG', 'camisa', 'camisa3.jpg'),
-(16, 'POKI', 'Multiplayer', 'jogo', 'jogo1.jpg'),
-(17, 'JM1', 'Jogos motais, Terror', 'jogo', 'jogo2.jpg'),
-(18, 'angrybird', 'Infantil', 'jogo', 'jogo3.jpg'),
-(19, 'samsung', 'Cinza', 'notebook', 'pc2.jpg'),
-(20, 'dell', 'Cinza', 'notebook', 'pc3.jpg');
+INSERT INTO `informatica` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
+(1, 'Pen Driver sandisk', '8GB, Preto e vermelho', 'informatica', 'info1.jpg'),
+(2, 'Pen Drive Multilaser ', '16GB', 'informatica', 'info2.jpg'),
+(3, 'Pen Drive Sony', '8GB, Preto', 'informatica', 'info3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `jogo`
+--
+
+CREATE TABLE `jogo` (
+  `id` int(4) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_bin NOT NULL,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `categoria` varchar(255) COLLATE utf8_bin NOT NULL,
+  `foto` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `jogo`
+--
+
+INSERT INTO `jogo` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
+(1, 'POKI', 'Aventura', 'jogo', 'jogo1.jpg'),
+(2, 'Jogos Mortais', 'Terror', 'jogo', 'jogo2.jpg'),
+(3, 'Angry Birds', 'Infantil', 'jogo', 'jogo3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livro`
+--
+
+CREATE TABLE `livro` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_bin NOT NULL,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `categoria` varchar(255) COLLATE utf8_bin NOT NULL,
+  `foto` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `livro`
+--
+
+INSERT INTO `livro` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
+(1, 'Illuminati', 'Ficcao Cientifica', 'livro', 'livro1.jpg'),
+(2, 'Imaginacao', 'Aventura', 'livro', 'livro2.jpg'),
+(3, 'Virando o Jogo', 'Acao', 'livro', 'livro3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `roupa`
+--
+
+CREATE TABLE `roupa` (
+  `id` int(4) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_bin NOT NULL,
+  `descricao` varchar(255) COLLATE utf8_bin NOT NULL,
+  `categoria` varchar(255) COLLATE utf8_bin NOT NULL,
+  `foto` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `roupa`
+--
+
+INSERT INTO `roupa` (`id`, `nome`, `descricao`, `categoria`, `foto`) VALUES
+(1, 'Camisa Black', 'Preta, GG', 'roupa', 'camisa1.jpg'),
+(2, 'Casaco Black', 'Preto, GG', 'roupa', 'camisa2.jpg'),
+(3, 'Camisa Colorida', 'Colorida, GG', 'roupa', 'camisa3.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `celular`
+--
+ALTER TABLE `celular`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `cliente`
 --
 ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `computador`
+--
+ALTER TABLE `computador`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -104,9 +214,27 @@ ALTER TABLE `fucionario`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `produto`
+-- Indexes for table `informatica`
 --
-ALTER TABLE `produto`
+ALTER TABLE `informatica`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jogo`
+--
+ALTER TABLE `jogo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roupa`
+--
+ALTER TABLE `roupa`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -114,24 +242,45 @@ ALTER TABLE `produto`
 --
 
 --
+-- AUTO_INCREMENT for table `celular`
+--
+ALTER TABLE `celular`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+--
+-- AUTO_INCREMENT for table `computador`
+--
+ALTER TABLE `computador`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `fucionario`
 --
 ALTER TABLE `fucionario`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
 --
--- AUTO_INCREMENT for table `produto`
+-- AUTO_INCREMENT for table `informatica`
 --
-ALTER TABLE `produto`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-COMMIT;
-
+ALTER TABLE `informatica`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `jogo`
+--
+ALTER TABLE `jogo`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `roupa`
+--
+ALTER TABLE `roupa`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
