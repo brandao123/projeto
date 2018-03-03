424437d7e5b49b78c63f933e62b889916356b3af
brandao123 <gabrielcandidobrandao@gmail.com>
2018-03-02 21:19:43 -0300

    adiciona pagina status

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ include 'conexao.php';
 $status = $_POST['status'];
 $id = $_POST['id'];
 
-$sql = "UPDATE pedido SET status='$status' WHERE id=$id";
+$sql = "UPDATE pedido SET id='$id' WHERE status=$status";
 
 if ($conn->query($sql) === TRUE) {
 	echo "Atualizado";
